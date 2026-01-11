6402683a7ba5077bf56246c73aad63ccd3f7811a
petrakiio <eup0022@gmail.com>
2026-01-11 18:38:45 -0500

    Logica feita

--- Aulas/aula3.js
+++ Aulas/aula3.js
@@ -3,17 +3,21 @@
 function adicionarTarefa(){
     let input = document.getElementById('input-tarefa')
     let tarefa = input.value
-    document.getElementById('mensagem').textContent='Tarefa Adicionada:'+tarefa
-
-    let Listatarefas = document.getElementById('listaTarefas')
-    //cria elemento li
-    let novaTarefa = document.createElement('li')
     
-    //Adicionando valor do input
-    novaTarefa.textContent=tarefa
+    if (tarefa === ''){
+        document.getElementById('mensagem').textContent='Tarefa vazia,erro ao adicionar'
+    }else{
+        document.getElementById('mensagem').textContent='Tarefa Adicionada:'+tarefa
+        let Listatarefas = document.getElementById('listaTarefas')
+        //cria elemento li
+        let novaTarefa = document.createElement('li')
+        
+        //Adicionando valor do input
+        novaTarefa.textContent=tarefa
 
-    //Adiciona como filho
-    Listatarefas.appendChild(novaTarefa)
+        //Adiciona como filho
+        Listatarefas.appendChild(novaTarefa)
 
-    tarefa.value = ''
+        tarefa.value = ''
+    }
 }
