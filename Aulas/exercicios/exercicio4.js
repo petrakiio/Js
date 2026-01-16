0a985ceac69e688aea7e67e710e1d06765e9effe
petrakiio <eup0022@gmail.com>
2026-01-15 21:01:42 -0500

    terminando logica

--- Aulas/exercicios/exercicio4.js
+++ Aulas/exercicios/exercicio4.js
@@ -1,10 +1,27 @@
 const card = document.getElementById('grid')
 const n = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 n.sort(() => Math.random() - 0.5)
-const valor =''
 
-for(valor in n){
+let primeirocard = null
+let segundocard = null
+let travado = false
+
+for(const valor of n){
     const bloco = document.createElement('div')
-    bloco.className='card'
+    bloco.classList.add('card')
+    bloco.dataset.valor = valor
+    bloco.textContent=''
+
+    bloco.addEventListener('click', ()=>verificar(bloco))
+
     card.appendChild(bloco)
+}
+
+function verificar(bloco){
+    let card_click = bloco.dataset.valor
+    primeirocard = card_click
+    segundocard = card_click
+    bloco.classList.add('revelado')
+    bloco.textContent = card_click
+    console.log(primeirocard,segundocard)
 }
