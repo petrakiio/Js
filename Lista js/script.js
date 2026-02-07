2a4532dc203bfaacd24b2b965f3bef497b8681fb
petrakiio <eup0022@gmail.com>
2026-02-06 20:35:59 -0500

    Tentando deixar o codigo mais limpo

--- Lista js/script.js
+++ Lista js/script.js
@@ -1,22 +1,95 @@
-const msg = document.getElementById("mensagem")
-const lista = document.getElementById("listaTarefas")
-const btn = document.getElementById("adcTarefa")
+const btnt = document.getElementById('adcTarefa')
+const mensagem = document.getElementById("mensagem")
+let tarefas = []
 
-function criarTarefa(tarefa) {
-    let li = document.createElement("li")
-    li.textContent = tarefa
-    lista.appendChild(li)
+btnt.addEventListener('click', () =>{
+    const inputTarefa = document.getElementById("input-tarefa")
+    let tarefa = inputTarefa.value.trim()
+
+
+
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro
+        mensagem.className='erro'
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+        mensagem.className='pass'
+        tarefas.push(tarefa)
+        renderizarTarefas()
+    }
+
+    inputTarefa.value = ""
+})
+
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML = ""
+
+    for (let i = 0 ; i < tarefas.length; i++){
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefas[i]
+        //botão de remover
+        removebtn()
+        novaTarefa.appendChild(removebtn)
+        //botão de editar
+        editarbtn()
+
+        novaTarefa.appendChild(editorbtn)
+        listaTarefas.appendChild(novaTarefa)
+    }
+    if (tarefas.length === 3){
+            criarbtn()
+        }
 }
 
-btn.addEventListener("click", function() {
-    let tarefa = document.getElementById('input-tarefa').value.trim()
-    if (tarefa === ""){
-        msg.textContent = "Por favor, insira uma tarefa."
-        msg.className="error"
+function removebtn(){
+    const removebtn = document.createElement('button')
+        removebtn.textContent='Remover'
+        removebtn.className='remover'
+        removebtn.onclick= () => removertarefa(i) //arrow function
+}
+function editarbtn(){
+    const editorbtn = document.createElement('button')
+        editorbtn.textContent='Editar'
+        editorbtn.className='editar'
+        editorbtn.onclick = () => editartarefa(i)
+}
+
+function criarbtn(){
+    //evita botões duplicados
+    if (document.querySelector('.botao_lista')) return;
+
+    const btnL = document.createElement('button')
+    btnL.textContent='Esvaziar lixeira'
+    btnL.onclick = () => esvaziar()
+    btnL.className='botao_lista'
+
+    //adicionar no .container
+    document.querySelector('.container').appendChild(btnL)
+}
+
+function esvaziar(){
+    tarefas.length = 0
+    mensagem.textContent='Lista limpa'
+    mensagem.className='pass'
+    const btn = document.querySelector('.botao_lista')
+    if (tarefas.length === 0) btn.remove() 
+    renderizarTarefas()
+}
+
+function removertarefa(indice){
+    tarefas.splice(indice, 1)
+    renderizarTarefas()
+}
+function editartarefa(indece){
+    const tarefaEditada = prompt('Edite a sua tarefa').trim()
+    if (tarefaEditada === ''){
+        mensagem.textContent='Coloque um novo nome pra editar!'
+        mensagem.className='erro'
     }else{
-        criarTarefa(tarefa)
-        msg.textContent = "Tarefa adicionada com sucesso!"
-        msg.className="pass"
-        document.getElementById('input-tarefa').value = ""
+        tarefas[indece] = tarefaEditada
+        renderizarTarefas()
     }
-})
+}
